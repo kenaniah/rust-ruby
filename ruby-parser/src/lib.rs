145ab50b4f711805ee4f985633bd7f3c30c7cb66
Kenaniah Cerny <kenaniah@gmail.com>
2019-09-02 12:31:56 -0700

    checkpoint

--- ruby-parser/src/lib.rs
+++ ruby-parser/src/lib.rs
@@ -8,11 +8,12 @@ mod tests {
     use super::*;
     #[test]
     fn it_works() {
-        assert!(ruby26::InputElementParser::new().parse("foo").is_err());
+        assert!(ruby26::InputElementParser::new().parse("foo").is_ok());
+        assert!(ruby26::InputElementParser::new().parse("`foo").is_err());
     }
     #[test]
     fn second() {
-        assert!(ruby26::InputElementParser::new().parse("\n").is_ok());
+        assert!(ruby26::InputElementParser::new().parse("foo\n").is_ok());
         assert!(ruby26::InputElementParser::new().parse("\r\n__END__asldfjdsalfk").is_ok());
     }
 }
