3541d1fa2c79427034ebd10d4b92084d572d12d4
Kenaniah Cerny <kenaniah@gmail.com>
2019-10-07 22:36:20 -0700

    fmt

--- ruby-lexer/src/tokens.rs
+++ ruby-lexer/src/tokens.rs
@@ -63,25 +63,43 @@ pub enum Token {
     // 8.4 - Whitespace
     Whitespace, // tab (0x09), vertical tab (0x0b), form feed (0x0c), carriage return (0x0d), space (0x20)
     // 8.5 - Comments
-    Comment { value: String },
+    Comment {
+        value: String,
+    },
     // 8.6 - End of program markers
     EndOfProgramMarker, // __END__
     // 8.7.3 - Identifiers
     /// **Original Grammar:** `tIDENTIFIER`
-    Identifier { value: String },
+    Identifier {
+        value: String,
+    },
     /// **Original Grammar:** `tGVAR`
-    GlobalVariable { value: String },
+    GlobalVariable {
+        value: String,
+    },
     /// **Original Grammar:** `tCVAR`
-    ClassVariable { value: String },
+    ClassVariable {
+        value: String,
+    },
     /// **Original Grammar:** `tIVAR`
-    InstanceVariable { value: String },
+    InstanceVariable {
+        value: String,
+    },
     /// **Original Grammar:** `tCONSTANT`
-    Constant { value: String },
+    Constant {
+        value: String,
+    },
     /// **Original Grammar:** `tFID`
-    FunctionIdentifier { value: String },
+    FunctionIdentifier {
+        value: String,
+    },
     /// **Original Grammar:** `tLABEL_TAG`
-    LabelTag { value: String },
-    AssignmentLikeMethodIdentifier { value: String },
+    LabelTag {
+        value: String,
+    },
+    AssignmentLikeMethodIdentifier {
+        value: String,
+    },
     // 8.7.4 - Punctuators
     LeftBracket,   // [
     RightBracket,  // ]
@@ -130,12 +148,18 @@ pub enum Token {
     OpElementGet,    // []
     OpElementSet,    // []=
     // Operator assignment methods
-    AssignmentOperator { value: String },
+    AssignmentOperator {
+        value: String,
+    },
     // 8.7.6 - Literals
     /// **Original Grammar:** `tINTEGER`
-    Integer { value: isize },
+    Integer {
+        value: isize,
+    },
     /// **Original Grammar:** `tFLOAT`
-    Float { value: f64 },
+    Float {
+        value: f64,
+    },
     /// **Original Grammar:** `tCHAR`
     ///
     /// Represents the `?<char>` character literal notation that can be used to build single character strings.
@@ -148,29 +172,52 @@ pub enum Token {
     /// * `?\M-\C-x` - Control + Meta + `x`
     ///
     /// See [Ruby's string literal syntax](https://github.com/ruby/ruby/blob/trunk/doc/syntax/literals.rdoc#strings) for more info.
-    Char { value: String },
-    Complex { real: f64, imag: f64 },
+    Char {
+        value: String,
+    },
+    Complex {
+        real: f64,
+        imag: f64,
+    },
     /// **Original Grammar:** `tXSTRING`
     ///
     /// Represents a backtick string (which captures the result of a subshell). Backtick strings come in two forms:
     /// * `` `backtick string` ``
     /// * `%{backtick string}`
-    XString { value: String },
+    XString {
+        value: String,
+    },
     /// **Original Grammar:** `tSTRING`
-    String { value: String },
+    String {
+        value: String,
+    },
     /// **Original Grammar:** `tSTRING_PART`
-    StringPart { value: String },
+    StringPart {
+        value: String,
+    },
     /// **Original Grammar:** `tSTRING_MID`
-    StringMid { value: String },
+    StringMid {
+        value: String,
+    },
     /// **Original Grammar:** `tREGEXP`
-    Regex { value: String },
+    Regex {
+        value: String,
+    },
     /// **Original Grammar:** `tNTH_REF`
-    RegexNthRef { value: String },
+    RegexNthRef {
+        value: String,
+    },
     /// **Original Grammar:** `tBACK_REF`
-    RegexBackRef { value: String },
-    Symbol { value: String },
+    RegexBackRef {
+        value: String,
+    },
+    Symbol {
+        value: String,
+    },
     // Things that need refactoring down the line
-    RefactorIdentifier { value: String },
+    RefactorIdentifier {
+        value: String,
+    },
     // Character tokens
     At,        // @
     Dot,       // .
