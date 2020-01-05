6361c1b2c76c9f36b1416fe3a97157fb63d7b051
Kenaniah Cerny <kenaniah@gmail.com>
2020-01-04 16:15:17 -0800

    docs

--- ruby-proc-macros/src/lib.rs
+++ ruby-proc-macros/src/lib.rs
@@ -2,6 +2,7 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 #[proc_macro_attribute]
+/// Provides a way to document where something may be found in an alternate implemntation of Ruby
 pub fn maps_to(_attr: TokenStream, item: TokenStream) -> TokenStream {
     // This macro should eventually add a section to the item's documentation that displays
     // references to other Ruby implementations
